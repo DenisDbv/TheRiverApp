14a34c19ce47413fad4e6fda2ae4ddebd1f1656a
denisdbv <denisdbv@gmail.com>
2013-09-01 23:42:42 +0400

    Init App

---
+++ TheRiverApp/TRAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TRAppDelegate.h
+//  TheRiverApp
+//
+//  Created by DenisDbv on 01.09.13.
+//  Copyright (c) 2013 axbx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TheRiverApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TheRiverApp
+//
+//  Created by DenisDbv on 01.09.13.
+//  Copyright (c) 2013 axbx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TRAppDelegate class]));
+    }
+}
