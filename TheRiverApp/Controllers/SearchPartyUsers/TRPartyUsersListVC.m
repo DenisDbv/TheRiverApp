0c124c6e8ed25c89f8ea2f4f084c4e9c6d5fc607
denisdbv <denisdbv@gmail.com>
2013-09-24 00:54:56 +0400

    Создание контроллера поиска и итема в левом меню

---
+++ TheRiverApp/Controllers/SearchPartyUsers/TRPartyUsersListVC.m
@@ -0,0 +1,73 @@
+//
+//  TRPartyUsersListVC.m
+//  TheRiverApp
+//
+//  Created by DenisDbv on 24.09.13.
+//  Copyright (c) 2013 axbx. All rights reserved.
+//
+
+#import "TRPartyUsersListVC.h"
+
+@interface TRPartyUsersListVC ()
+
+@end
+
+@implementation TRPartyUsersListVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 100;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 109;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *itemCellIdentifier = @"TRMindCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:itemCellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:itemCellIdentifier];
+    }
+    
+    cell.textLabel.text = @"vfsvdfv";
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView selectRowAtIndexPath:indexPath animated:NO scrollPosition:UITableViewScrollPositionNone];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+@end
