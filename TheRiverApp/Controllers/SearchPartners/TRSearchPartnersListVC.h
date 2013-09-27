2f398d8cf897f9e5d6b759040fed4e5c2f984ea1
denisdbv <denisdbv@gmail.com>
2013-09-27 23:58:33 +0400

    Вход в контроллер поиска по нажатию на кнопку в навбаре. Тип входа push controller

---
+++ TheRiverApp/Controllers/SearchPartners/TRSearchPartnersListVC.h
@@ -0,0 +1,15 @@
+//
+//  TRSearchPartnersListVC.h
+//  TheRiverApp
+//
+//  Created by DenisDbv on 27.09.13.
+//  Copyright (c) 2013 axbx. All rights reserved.
+//
+
+#import "TRCenterRootController.h"
+
+@interface TRSearchPartnersListVC : UIViewController
+
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+
+@end
---
+++ TheRiverApp/Controllers/SearchPartners/TRSearchPartnersListVC.m
@@ -0,0 +1,48 @@
+//
+//  TRSearchPartnersListVC.m
+//  TheRiverApp
+//
+//  Created by DenisDbv on 27.09.13.
+//  Copyright (c) 2013 axbx. All rights reserved.
+//
+
+#import "TRSearchPartnersListVC.h"
+#import "UIBarButtonItem+BarButtonItemExtended.h"
+
+@interface TRSearchPartnersListVC ()
+
+@end
+
+@implementation TRSearchPartnersListVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIBarButtonItem *onCancelButton = [UIBarButtonItem barItemWithImage:[UIImage imageNamed:@"toolbar-back-button@2x.png"] target:self action:@selector(onBack)];
+    [onCancelButton setBackgroundImage:[UIImage new] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [self.navigationItem setLeftBarButtonItem:onCancelButton animated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void) onBack
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+@end
