6267778d4702a073274c92759c051e132a5e9dd7
denisdbv <denisdbv@gmail.com>
2013-10-02 12:54:30 +0400

    Поддержка etag в запросе списка контактов для связи

--- TheRiverApp/Model/Network/Managers/TRContactsManager.m
+++ TheRiverApp/Model/Network/Managers/TRContactsManager.m
@@ -14,6 +14,8 @@
 
 @implementation TRContactsManager
 
+static const NSString * _fileHandler = @"contacts.data";
+
 + (instancetype)client
 {
     return [ABMultiton sharedInstanceOfClass:[self class]];
@@ -39,25 +41,58 @@
                                  kTGTokenKey,
                                  [TRAuthManager client].iamData.token];
     
+    
     URLDownloaderOperation * operation = [[URLDownloaderOperation alloc] initWithUrlString: urlContactList
+                                                                                      eTag:[self lastEtag]
                                                                           withSuccessBlock:^(LRRestyResponse *response) {
                                                                               
                                                                               NSDictionary *resultJSON = [[response asString] objectFromJSONString];
                                                                               
                                                                               TRContactsListModel *cotactListModel = [[TRContactsListModel alloc] initWithDictionary:resultJSON];
-                                                                              NSLog(@"%@", cotactListModel);
+                                                                              
+                                                                              NSMutableDictionary *storeContactsData = [[NSMutableDictionary alloc] init];
+                                                                              [storeContactsData setObject:[response.headers valueForKey:@"ETag"] forKey:@"etag"];
+                                                                              [storeContactsData setObject:resultJSON forKey:@"data"];
+                                                                              [self saveUserData:storeContactsData];
                                                                               
                                                                               if( successBlock != nil)
                                                                                   successBlock(response, cotactListModel);
                                                                               
                                                                           } andFailedBlock:^(LRRestyResponse *response){
                                                                               
-                                                                              if(failedOperation != nil)
-                                                                                  failedOperation(response);
-                                                                              
-                                                                              NSLog(@"Error auth: %@", response.asString);
+                                                                              if(response.status == 304)
+                                                                              {
+                                                                                  NSLog(@"Contact list not modified");
+                                                                                  
+                                                                                  if( successBlock != nil)
+                                                                                      successBlock(response, [self lastContactArray]);
+                                                                              }
+                                                                              else  {
+                                                                                  if(failedOperation != nil)
+                                                                                      failedOperation(response);
+                                                                                  
+                                                                                  NSLog(@"Error get contacts list: %@", response.description);
+                                                                              }
                                                                           }];
     
     [_queueContacts addOperation:operation];
+}
+
+-(void) saveUserData:(id)dataModel
+{
+    [[TGArhiveObject class] saveArhiveFromObject:dataModel toFile: (NSString*)_fileHandler];
+}
+
+-(NSString*) lastEtag
+{
+    NSMutableDictionary *contactsDictionary = [[TGArhiveObject class] unarhiveObjectFromFile: (NSString*)_fileHandler];
+    return [contactsDictionary objectForKey:@"etag"];
+}
+
+-(TRContactsListModel*) lastContactArray
+{
+    NSMutableDictionary *contactsDictionary = [[TGArhiveObject class] unarhiveObjectFromFile: (NSString*)_fileHandler];
+    TRContactsListModel *cotactListModel = [[TRContactsListModel alloc] initWithDictionary: [contactsDictionary objectForKey:@"data"]];
+    return cotactListModel;
 }
 @end
