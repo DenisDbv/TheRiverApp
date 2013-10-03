3c6bf11d9a890036e6a844fff710301aa06b99ac
denisdbv <denisdbv@gmail.com>
2013-10-03 09:46:14 +0400

    переход на сервер bmreki.ru

--- TheRiverApp/Model/Network/TRNetworkConstants.m
+++ TheRiverApp/Model/Network/TRNetworkConstants.m
@@ -10,18 +10,18 @@
 
 #pragma mark - Авторизация и ключи для авторизации
 // Авторизация
-NSString *const kTG_API_AuthUrl = @"http://kostum5.ru/api/login/";
+NSString *const kTG_API_AuthUrl = @"http://bmreki.ru/api/login/";
 // Ключи для авторизации
 NSString *const kTGUserLoginKey = @"email";
 NSString *const kTGUserPasswordKey = @"password";
 
 
-NSString *const kTG_API_CitiesList = @"http://kostum5.ru/api/get_city_list/";
-NSString *const kTG_API_IndustryList = @"http://kostum5.ru/api/get_scope_work_list/";
-NSString *const kTG_API_PartyUsersList = @"http://kostum5.ru/api/search_1_method/";
-NSString *const kTG_API_PartnersList = @"http://kostum5.ru/api/search_2_method/";
-NSString *const kTG_API_BusinessList = @"http://kostum5.ru/api/get_all_business/";
-NSString *const kTG_API_ContactList = @"http://kostum5.ru/api/get_player_contact_list/";
+NSString *const kTG_API_CitiesList = @"http://bmreki.ru/api/get_city_list/";
+NSString *const kTG_API_IndustryList = @"http://bmreki.ru/api/get_scope_work_list/";
+NSString *const kTG_API_PartyUsersList = @"http://bmreki.ru/api/search_1_method/";
+NSString *const kTG_API_PartnersList = @"http://bmreki.ru/api/search_2_method/";
+NSString *const kTG_API_BusinessList = @"http://bmreki.ru/api/get_all_business/";
+NSString *const kTG_API_ContactList = @"http://bmreki.ru/api/get_player_contact_list/";
 
 NSString *const kTGTokenKey = @"token";
 NSString *const kTGCityKey = @"city";
